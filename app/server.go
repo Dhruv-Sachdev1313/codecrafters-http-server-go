9a111e93fc7ff1c12a27c73ca9ae5aeeee9eba2a
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2024-07-12 20:59:12 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -16,15 +16,7 @@ func returnResponse(conn net.Conn, message string) {
 	}
 }
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
+func handleTCPRequest(l net.Listener) {
 	conn, err := l.Accept()
 	defer conn.Close()
 	if err != nil {
@@ -63,5 +55,17 @@ func main() {
 	} else {
 		returnResponse(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
 	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+	go handleTCPRequest(l)
 
 }
