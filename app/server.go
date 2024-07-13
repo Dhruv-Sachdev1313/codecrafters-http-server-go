14defe29c54152100f4258ef4ad49ee9b3e10710
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2024-07-13 19:40:25 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -75,7 +75,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 	var dir string
-	flag.StringVar(&dir, "directory", "", "Directory to serve files from")
+	flag.StringVar(&dir, "-directory", "", "Directory to serve files from")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
