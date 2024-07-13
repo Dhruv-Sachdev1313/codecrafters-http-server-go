c40b97982f8d96de00e2fdf12001c31bebd59265
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2024-07-13 19:04:35 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -62,12 +62,14 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	defer conn.Close()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		defer conn.Close()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleTCPRequest(conn)
 	}
-	go handleTCPRequest(conn)
 
 }
